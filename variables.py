075e0aea9bd74851cce1348d85eccda20962688d
Jacqueline192837 <zS18003256@estudiantes.uv.mx>
2021-03-30 21:54:38 +0000

    add variables

---
+++ variables.py
@@ -0,0 +1,23 @@
+# Variables.py
+# Author: Jacqueline Castillo
+# March 26, 2021
+
+year = 2021;
+sentence = "My name is jcastillo";
+sarah, bob, mike = 16, 20, 25;
+name, age = "maria", 24;
+mary = fer = 20;
+PI = 3.1416;
+e = 2.71;
+
+# Display values
+
+print "year = ", year;
+print sentence;
+print "Sarah : ", sarah, "\nBob   : ", bob, "\nMike  : ", mike;
+print "\n\t name : ", name;
+print "\n\t age : ", age;
+print mary, fer;
+print PI;
+print e;
+
